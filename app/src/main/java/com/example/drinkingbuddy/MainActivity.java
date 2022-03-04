05080aea2e6eb903859ca48501c7ba99b7598137
SimeonAtanasovGit <59778468+SimeonAtanasovGit@users.noreply.github.com>
2022-03-03 21:10:49 -0500

    Testing New Code

--- app/src/main/java/com/example/drinkingbuddy/MainActivity.java
+++ app/src/main/java/com/example/drinkingbuddy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.drinkingbuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "THIS SHIT WORKS!!!!!!!", Toast.LENGTH_LONG).show();
     }
 }
