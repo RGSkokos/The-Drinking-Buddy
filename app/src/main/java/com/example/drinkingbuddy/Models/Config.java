f861838c571f9312169e84dbbfc2c0c56d695989
Mihai Olaru <mihai.c.olaru@gmail.com>
2022-03-21 18:51:57 -0400

    fixed some bugs to show icons

--- app/src/main/java/com/example/drinkingbuddy/Models/Config.java
+++ app/src/main/java/com/example/drinkingbuddy/Models/Config.java
@@ -2,20 +2,20 @@ package com.example.drinkingbuddy.Models;
 
 public class Config {
 
-    public static final String DATABASE_NAME = "Breathalyzer_Result";
+    public static final String DATABASE_NAME = "BreathalyzerResult";
     public static final int DATABASE_VERSION = 1;
-    public static final String TABLE_NAME = "Results_table";
-    public static final String Result = "Result_Of_Measurement";
+    public static final String TABLE_NAME = "ResultsTable";
+    public static final String Result = "_ResultOfMeasurement";
     public static final String TimeStamp = "TimeStamp";
 
-    public static final String TABLE_NAME_PROFILE = "profile_table";
-    public static final String ID = "profile_id";
-    public static final String USERNAME = "profile_user";
-    public static final String PASSWORD = "profile_password";
-    public static final String DEVICE_NAME = "profile_device";
-    public static final String DEVICE_CODE = "profile_code";
-    public static final String DAY_OF_WEEK = "day_of_week";
+    public static final String TABLE_NAME_PROFILE = "profileTable";
+    public static final String ID = "_profileId";
+    public static final String USERNAME = "profileUser";
+    public static final String PASSWORD = "profilePassword";
+    public static final String DEVICE_NAME = "profileDevice";
+    public static final String DEVICE_CODE = "profileCode";
+    public static final String DAY_OF_WEEK = "dayOfWeek";
 
-    public static final String TYPE_OF_DRINK = "type_of_drink";
-    public static final String TABLE_NAME_DRINK_TYPE = "drink_type_table";
+    public static final String TYPE_OF_DRINK = "typeOfDrink";
+    public static final String TABLE_NAME_DRINK_TYPE = "drinkTypeTable";
 }
